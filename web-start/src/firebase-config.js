afaa16290384ac08088a9677025141189eca86ef
Sihan Tao JMC UG <sihan.tao20@imperial.ac.uk>
2022-06-09 11:24:55 +0100

    firebase setup

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -6,14 +6,21 @@
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
  */
-const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+const config =
+{
+  apiKey: "AIzaSyAuTrJntXrA0IV3gKpAz2_BhG9cC8t373E",
+  authDomain: "friendlychat-e7049.firebaseapp.com",
+  projectId: "friendlychat-e7049",
+  storageBucket: "friendlychat-e7049.appspot.com",
+  messagingSenderId: "896302854425",
+  appId: "1:896302854425:web:bdf461725ce1b7ba1bd588",
+  measurementId: "G-KKL9H8L0Y2"
 };
 
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
     throw new Error('No Firebase configuration object provided.' + '\n' +
-    'Add your web app\'s configuration object to firebase-config.js');
+      'Add your web app\'s configuration object to firebase-config.js');
   } else {
     return config;
   }
